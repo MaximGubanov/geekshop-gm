720bdd0d89bcad5854c463aaf451ead0c7998a8f
MaximGubanov <m_gubanov@bk.ru>
2021-10-21 14:08:20 +0300

    Урок 5: Добавление корзины

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from mainapp.models import Product, Contacts
+from basketapp.models import Basket
 
 
 def index(request):
     title = 'Магазин'
+    basket = Basket.objects.filter(user=request.user)
 
     products = Product.objects.all()[:3]
 
     context = {
         'title': title,
         'products': products,
+        'basket': basket,
     }
 
     return render(request, 'geekshop/index.html', context=context)
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,14 +1,36 @@
-from django.shortcuts import render
-from .models import ProductCategory
+from django.shortcuts import render, get_object_or_404
+from .models import ProductCategory, Product
 
-def products(request):
+
+def products(request, pk=None):
 
     title = 'Каталог'
-
+    products = Product.objects.all().order_by('price')
     links = ProductCategory.objects.all()
 
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+
+        context = {
+            'title': title,
+            'links': links,
+            'category': category,
+            'products': products,
+        }
+
+        return render(request, 'mainapp/products.html', context=context)
+
+    same_products = Product.objects.all()[1:2]
     context = {
         'title': title,
         'links': links,
+        'same_products': same_products,
+        'products': products,
     }
+
     return render(request, 'mainapp/products.html', context=context)
