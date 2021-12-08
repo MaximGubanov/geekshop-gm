0a0fa9d4758f56172c1bf40cad54296dc07733a1
MaximGubanov <m_gubanov@bk.ru>
2021-12-08 18:04:28 +0300

    lessons_2.8

--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -294,3 +294,20 @@ def product_delete(request, pk):
     }
 
     return render(request, 'adminapp/product_delete.html', context)
+
+
+def db_profile_by_type(prefix, type, queries):
+    update_queries = list(filter(lambda x: type in x['sql'], queries))
+    print(f'db_profile {type} for {prefix}: ')
+    [print(query['sql']) for query in update_queries]
+
+
+@receiver(pre_save, sender=ProductCategory)
+def product_is_active_update_productcategory_save(sender, instance, **kwargs):
+    if instance.pk:
+        if instance.is_active:
+            instance.product_set.update(is_active=True)
+        else:
+            instance.product_set.update(is_active=False)
+
+        db_profile_by_type(sender, 'UPDATE', connection.queries)
